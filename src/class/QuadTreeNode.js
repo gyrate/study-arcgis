a16481a62ccb08c130b402fe2963e3fd0c081466
gyrate <gyratesky@gmail.com>
2022-04-05 22:16:00 +0800

    diyCluster

--- src/class/QuadTreeNode.js
+++ src/class/QuadTreeNode.js
@@ -3,7 +3,7 @@ class QuadTreeNode {
 
   constructor(data, conf) {
     //节点范围
-    this.extent = conf.extent || {minX: 0, minY: 0, maxX: 100, maxY: 100}
+    this.extent = conf.extent || {xmin: 0, ymin: 0, xmax: 100, ymax: 100}
     // 节点数据容量
     this.bucketLimit = conf.bucketLimit || 10
     //存储的数据点
@@ -17,25 +17,29 @@ class QuadTreeNode {
     this.split()
   }
 
-  //数据点数量达到容量上限，则分裂子节点
+  /**
+   * 数据点数量达到容量上限，则分裂子节点
+   */
   split(){
     if (this.points.length > this.bucketLimit) {
       this.createChildren()
     }
   }
 
-  //创建子节点
+  /**
+   * 创建子节点
+   */
   createChildren() {
 
-    const {minX, minY, maxX, maxY} = this.extent
-    const h = maxY - minY
-    const w = maxX - minX
+    const {xmin, ymin, xmax, ymax} = this.extent
+    const h = ymax - ymin
+    const w = xmax - xmin
 
     //四个象限
-    this.northWest = new QuadTreeNode([], {extent: {minX: minX, minY: minY + h / 2, maxX: minX + w / 2, maxY: maxY}})
-    this.northEast = new QuadTreeNode([], {extent: {minX: minX + w / 2, minY: minY + h / 2, maxX: maxX, maxY: maxY}})
-    this.southWest = new QuadTreeNode([], {extent: {minX: minX, minY: minY, maxX: minX + w / 2, maxY: minY + h / 2}})
-    this.southEast = new QuadTreeNode([], {extent: {minX: minX + w / 2, minY: minY, maxX: maxX, maxY: minY + h / 2}})
+    this.northWest = new QuadTreeNode([], {extent: {xmin, ymin: ymin + h / 2, xmax: xmin + w / 2, ymax}})
+    this.northEast = new QuadTreeNode([], {extent: {xmin: xmin + w / 2, ymin: ymin + h / 2, xmax, ymax}})
+    this.southWest = new QuadTreeNode([], {extent: {xmin, ymin, xmax: xmin + w / 2, ymax: ymin + h / 2}})
+    this.southEast = new QuadTreeNode([], {extent: {xmin: xmin + w / 2, ymin, xmax, ymax: ymin + h / 2}})
 
     //填充数据
     this.points.forEach(p => {
@@ -51,7 +55,7 @@ class QuadTreeNode {
     })
     this.points = []
 
-    //分裂节点
+    //尝试分裂节点
     this.northWest.split()
     this.northEast.split()
     this.southWest.split()
@@ -64,11 +68,16 @@ class QuadTreeNode {
   }
 
 
-  //判断某个点是否在当前节点范围内
-  within(point) {
+  /**
+   * 判断某个点是否在当前节点范围内
+   * @param point
+   * @param extent 指定范围，如果不填则使用当前节点范围
+   * @returns {boolean}
+   */
+  within(point, extent) {
     const {x, y} = point
-    const {minX, minY, maxX, maxY} = this.extent
-    if ((minX <= x && maxX >= x) || (minY <= y && maxY >= y)) {
+    const {xmin, ymin, ymax, xmax} = extent || this.extent
+    if ((xmin <= x && xmax >= x) && (ymin <= y && ymax >= y)) {
       return true
     } else {
       return false
@@ -76,17 +85,26 @@ class QuadTreeNode {
   }
 
 
-  //判断当前节点范围是否与指定范围相交
+  /**
+   * 判断当前节点范围是否与指定范围相交
+   * 如果两个矩形相交，则两个矩形中心点间的距离肯定小于两个矩形边长和的1/2
+   * @param extent 指定范围
+   * @returns {boolean}
+   */
   intersects(extent) {
 
     const {xmin, ymin, xmax, ymax} = extent
-    const {minX, minY, maxX, maxY} = this.extent
 
-    if (
-      (minX <= xmin && maxX >= xmin) ||
-      (minX <= xmax && maxX >= xmax) ||
-      (minY <= ymin && maxY >= ymin) ||
-      (minY <= ymax && maxY >= ymax)) {
+    //x轴 两个矩形中心点距离 * 2
+    let lx = Math.abs(this.extent.xmax + this.extent.xmin - xmin - xmax)
+    //x轴 两个矩形边长和
+    let bx = Math.abs(this.extent.xmax - this.extent.xmin + xmax - xmin)
+    //y轴 两个矩形中心点距离 * 2
+    let ly = Math.abs(this.extent.ymax + this.extent.ymin - ymin - ymax)
+    //y轴 两个矩形x轴边长和
+    let by = Math.abs(this.extent.ymax - this.extent.ymin + ymax - ymin)
+
+    if (lx <= bx && ly <= by) {
       return true
     } else {
       return false
@@ -94,7 +112,10 @@ class QuadTreeNode {
   }
 
 
-  //判断当前节点是否叶子节点
+  /**
+   * 判断当前节点是否叶子节点
+   * @returns {boolean}
+   */
   isLeafNode() {
     if (this.northWest || this.northEast || this.southWest || this.southEast) {
       return false
@@ -103,15 +124,28 @@ class QuadTreeNode {
     }
   }
 
-
-  //查找指定范围内的坐标点
+  /**
+   * 查找指定范围内的坐标点
+   * @param extent 指定范围
+   * @returns {Array}
+   */
   findPoints(extent) {
+
+    const t = this
     let arr = []
+
     function travel(node) {
 
       if (node.intersects(extent)) {
-        if (node.isLeafNode()) {
-          arr = [...arr, ...node.points]
+
+        //如果当前四叉树节点有points，则将在extent范围内的points入栈
+        if (node.points.length > 0) {
+          node.points.forEach(point => {
+            if (t.within(point, extent)) {
+              arr.push(point)
+            }
+          })
+
         } else {
           const {northWest, northEast, southWest, southEast} = node
 
--- src/views/DiyCluster.vue
+++ src/views/DiyCluster.vue
@@ -14,7 +14,7 @@
   import FeatureLayer from '@arcgis/core/layers/FeatureLayer'
   import LabelClass from "@arcgis/core/layers/support/LabelClass"
 
-  import GraphicsLayer  from '@arcgis/core/layers/GraphicsLayer';
+  import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
   import Sketch from '@arcgis/core/widgets/Sketch'
 
   import QuadTreeNode from '@/class/QuadTreeNode'
@@ -23,7 +23,7 @@
   export default {
     name: 'DiyCluster',
     components: {},
-    data () {
+    data() {
       return {
         totalNum: 0,
         map: null,
@@ -31,19 +31,19 @@
         quadTree: null,
 
         filterList: [
-          {id: 0, count:0, label: '在线', value: 'ONLINE', active: true},
-          {id: 1, count:0, label: '离线', value: 'OFFLINE', active: true},
-          {id: 2, count:0, label: '未激活', value: 'UNACTIVATED', active: true},
+          {id: 0, count: 0, label: '在线', value: 'ONLINE', active: true},
+          {id: 1, count: 0, label: '离线', value: 'OFFLINE', active: true},
+          {id: 2, count: 0, label: '未激活', value: 'UNACTIVATED', active: true},
         ]
 
       }
     },
-    mounted () {
+    mounted() {
       this.initMap()
 
     },
     methods: {
-      initMap(){
+      initMap() {
         //创建地图
         const map = new Map({
           // basemap: "osm-standard" //矢量图
@@ -65,13 +65,19 @@
           view: view
         })
         view.ui.add(layerList, {
-          position: 'bottom-left'
+          position: 'bottom-right'
         })
 
         //view创建完成后开始监听事件
         view.when(async () => {
           this.initBind()
-          //创建图层
+
+          await this.getData()
+
+          //创建散点图层
+          this.initPointLayer()
+
+          //创建聚合图层
           this.initClusterLayer()
           //创建聚合类
           await this.initClusterClass()
@@ -79,8 +85,27 @@
           this.resetCluster()
         })
 
+        view.watch("zoom", (e) => {
+          if (this.view.zoom % 1 == 0) {
+            console.log(`zoom ${this.view.zoom}`)
+            this.resetCluster()
+          }
+        })
+
         this.map = map
         window._view = this.view = view
+      },
+
+      async getData() {
+        const res = await axios.get(`${process.env.BASE_URL}/static/mockData.json`)
+        this.data = res.data.data.map(item => {
+          const {id, location} = item
+          return {
+            id,
+            x: location[0],
+            y: location[1]
+          }
+        })
       },
 
       //鼠标事件监听
@@ -100,37 +125,61 @@
 
         })
 
-        this.view.on('mouse-wheel', (event)=>{
-          this.resetCluster()
-        })
-
       },
 
-      async initClusterClass(){
-
-        const res = await axios.get(`${process.env.BASE_URL}/static/mockData.json`)
-        const data = res.data.data
+      async initClusterClass() {
         const {xmin, ymin, xmax, ymax} = this.view.extent
         const [minX, minY] = webMercatorUtils.xyToLngLat(xmin, ymin)
         const [maxX, maxY] = webMercatorUtils.xyToLngLat(xmax, ymax)
 
         //创建四叉树
-        const points = data.map(item => {
-          const {id, location} = item
-          return {
-            id,
-            x: location[0],
-            y: location[1]
-          }
-        })
-        const tree = new QuadTreeNode(points, {extent: {minX, minY, maxX, maxY}})
+        const points = [...this.data]
+        const tree = new QuadTreeNode(points, {extent: {xmin:minX, ymin: minY, xmax: maxX, ymax: maxY}})
         this.quadTree = tree
       },
 
+      drawGrid(grids) {
+        console.log('drawGrid')
+        if (!this.asssetLayer) {
+          const layer = new GraphicsLayer({
+            title: '网格图层',
+            id: 'gridLayer',
+            graphics: []
+          })
+          this.map.add(layer)
+          this.asssetLayer = layer
+        }
+        this.asssetLayer.removeAll()
+
+        const lines = []
+        grids.forEach(item => {
+          const {xmin, ymin, xmax, ymax} = item
+          const line = new Graphic({
+            geometry: {
+              type: "polyline",
+              paths: [
+                [xmin, ymin],
+                [xmax, ymin],
+                [xmax, ymax],
+                [xmin, ymax],
+                [xmin, ymin],
+              ]
+            },
+            symbol: {
+              type: "simple-line",
+              color: '#f00',
+              width: 1
+            },
+            attributes: {}
+          })
+          lines.push(line)
+        })
+
+        this.asssetLayer.addMany(lines)
+      },
+
       //重置 聚合点
-      resetCluster(){
-
-
+      resetCluster() {
 
         const {xmin, ymin, xmax, ymax} = this.view.extent
         const [minX, minY] = webMercatorUtils.xyToLngLat(xmin, ymin)
@@ -141,14 +190,20 @@
         const h = (maxY - minY) / AMOUNT
         const w = (maxX - minX) / AMOUNT
         const grids = []
+
         for (let i = 0; i < AMOUNT; i++) {
-          grids.push({
-            xmin: minX + w * i,
-            xmax: minX + w * (i + 1),
-            ymin: minY + h * i,
-            ymax: minY + h * (i + 1)
-          })
+          for (let j = 0; j < AMOUNT; j++) {
+            grids.push({
+              xmin: minX + w * j,
+              xmax: minX + w * (j + 1),
+              ymin: minY + h * i,
+              ymax: minY + h * (i + 1)
+            })
+          }
         }
+
+        this.drawGrid(grids)
+
         // const grids = []
         // const UNIT = 0.2
         // for (let i = 0; minX + UNIT * i < maxX; i++) {
@@ -163,19 +218,26 @@
         // }
 
         let arr = []
-        grids.forEach(grid => {
+        grids.forEach((grid, index) => {
+
           const points = this.quadTree.findPoints(grid)
-          const point = this.mergetPoints(points)
-          //聚合单个网格上的点
-          arr = [...arr, point]
+          if (points.length > 0) {
+            const point = this.fusePoints(points, index)
+            arr.push(point)
+          }
         })
 
-        console.log(arr)
         this.updateClusterLayer(arr)
       },
 
-      ///聚合单个网格上的点
-      mergetPoints(points) {
+      /**
+       * 获取单个网格上的聚合点
+       * 聚合点为网格内所有点的平均质心
+       * @param points
+       * @param id
+       * @returns {{x: number, y: number, count: *, id: *}}
+       */
+      fusePoints(points, id) {
         let totalX = 0
         let totalY = 0
         let len = points.length
@@ -186,20 +248,57 @@
         return {
           x: totalX / len,
           y: totalY / len,
-          count: len
+          count: len,
+          id
         }
       },
 
-      async initClusterLayer() {
+      initPointLayer() {
+
+        const source = this.data.map(item => {
+          return this.createGraphic(item, 'point')
+        })
 
         let layer = new FeatureLayer({
-          id:'cluster1',
-          title:'聚合图层',
+          id: 'cluster1',
+          title: '聚合图层',
           outFields: ["*"],
           fields: [
             {name: "ObjectID", type: "string"},
             {name: "count", type: "string"},
             {name: "type", type: "string"},
+          ],
+          source,
+          visible: true,
+          objectIdField: "ObjectID",
+          geometryType: 'point',
+          renderer: {
+            type: "simple",
+            symbol: {
+              type: "simple-marker",
+              size: '10',
+              color: "#c3ff74",
+              outline: {
+                width: 0,
+                color: "#fff"
+              }
+            }
+          }
+        })
+        this.map.add(layer)
+        this.clusterLayer = layer
+      },
+
+      async initClusterLayer() {
+
+        let layer = new FeatureLayer({
+          id: 'cluster1',
+          title: '聚合图层',
+          outFields: ["*"],
+          fields: [
+            {name: "ObjectID", type: "string"},
+            {name: "count", type: "string"},
+            {name: "id", type: "string"},
           ],
           source: [],
           visible: true,
@@ -209,7 +308,7 @@
             type: "simple",
             symbol: {
               type: "simple-marker",
-              size: '30px',
+              size: '20',
               color: "#FF0F43",
               outline: {
                 width: 2,
@@ -217,14 +316,18 @@
               }
             }
           },
-          labelingInfo:[ new LabelClass({
-            labelPlacement:'center-center',
-            labelExpressionInfo: { expression: "$feature.count" },
+          labelingInfo: [new LabelClass({
+            labelPlacement: 'center-center',
+            // labelExpressionInfo: {expression: "$feature.id +','+ $feature.count"},
+            labelExpressionInfo: {expression: "$feature.count"},
             symbol: {
               type: "text",
               color: "#fff",
               haloSize: 1,
               haloColor: "#FF0F43"
+            },
+            font:{
+              weight: "bold"
             }
           })]
 
@@ -233,14 +336,14 @@
         this.clusterLayer = layer
       },
 
-      async updateClusterLayer(data){
+      async updateClusterLayer(data) {
 
         const layer = this.clusterLayer
 
         // 获取待清除的图层要素id
         const ObjectIds = await layer.queryObjectIds();
         const deleteFeatures = ObjectIds.map((v) => {
-          return { objectId: v };
+          return {objectId: v};
         })
 
         // 获取待添加的图层要素
@@ -249,14 +352,15 @@
         });
 
         //更新图层
-        const results = await layer.applyEdits({ deleteFeatures, addFeatures });
+        const results = await layer.applyEdits({deleteFeatures, addFeatures});
+        console.log(results)
       },
 
       createGraphic(item, type) {
 
         var props
 
-        switch(type){
+        switch (type) {
           case 'point':
             props = {
               longitude: item.x,
@@ -286,10 +390,6 @@
       },
 
 
-
-
-
-
     }
   }
 </script>
@@ -301,11 +401,11 @@
 </style>
 <style lang="scss" type="text/scss" scoped>
 
-  .map-point-tip{
+  .map-point-tip {
     position: absolute;
-    top:-9999px;
+    top: -9999px;
     left: -9999px;
-    padding: .4em ;
+    padding: .4em;
     border-radius: 4px;
     background-color: #0f286a;
     transition: all .3s;
